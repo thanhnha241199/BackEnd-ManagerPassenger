a96f4fadd5425e5a6e01d3f3af998d517a52d258
Thanh Nha <huynhthanhnha24111999@gmail.com>
2021-03-11 15:38:44 +0700

    Update

--- methods/actions.js
+++ methods/actions.js
@@ -86,7 +86,9 @@ var functions = {
                                     success: true, 
                                     token: token,
                                     type: user.type,
-                                    email: user.email
+                                    email: user.email,
+                                    name: user.name,
+                                    id: user._id
                                 })
                         }
                         else {
